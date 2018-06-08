c3b509b18536ef8c3fa9768d3f539b50b1f0066a
lujie <lujiefsi@foxmail.com>
2018-06-08 14:38:35 +0800

    change README and add ex6-12

---
+++ LPython/LPTHW/ex13.py
@@ -0,0 +1,9 @@
+# argv
+from  sys import argv
+
+script, first, scond, third = argv
+print "%r" % argv
+print "The script is called:", script
+print "Your first variable is ", first
+print "Your scond variable is ", scond
+print "Your third variable is ", third
---
+++ LPython/LPTHW/ex14.py
@@ -0,0 +1,10 @@
+# prompting
+from sys import argv
+
+script, user_name = argv
+prompt = ">"
+
+print "Hi %s, I am the %s scirpt" % (user_name, script)
+print "Do you like me %s" % user_name
+likes = raw_input(prompt)
+print likes
