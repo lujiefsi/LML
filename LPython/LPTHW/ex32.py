29dd874cacf954fb1eda0c92887be6043269e81f
lujie <lujiefsi@foxmail.com>
2018-06-25 20:32:14 +0800

    add ex32-33

---
+++ LPython/LPTHW/ex32.py
@@ -0,0 +1,19 @@
+the_count = [1,2,3,4,6]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarter']
+
+for number in the_count:
+    print ("This count", number)
+for fruit in fruits:
+    print("a fruits of type", fruit)
+for i in change:
+    print("i got change %r" %i)
+
+elements = []
+
+for i in range(0,6):
+    print("Adding %d to the list." %i)
+    elements.append(i)
+
+for i in elements:
+    print("eclments was %d" %i)
---
+++ LPython/LPTHW/ex33.py
@@ -0,0 +1,14 @@
+'''
+Created on Jun 25, 2018
+
+@author: lujie
+'''
+i = 0
+numbers = []
+
+while i< 6:
+    print("At the top i is %d" %i)
+    numbers.append(i)
+    i = i+1
+    print("number now:",numbers)
+    print("at the bottom number is:",i)
