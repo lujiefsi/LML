2a01bff4f9c3ab5654b915163ffa1c9b5e755c84
lujie <lujiefsi@foxmail.com>
2018-12-11 17:12:38 +0800

    none

--- tensorflow/Feed.py
+++ tensorflow/Feed.py
@@ -1,8 +1,15 @@
+import os
+import time
 import tensorflow as tf
+from tensorflow.contrib.compiler import jit
+os.environ['CUDA_VISIBLE_DEVICES'] = ''
+print (os.getpid())
 
 input1 = tf.placeholder(tf.float32)
 input2 = tf.placeholder(tf.float32)
-output = tf.multiply(input1, input2)
+jit_scope = jit.experimental_jit_scope
+with jit_scope(compile_ops=True):
+	output = tf.multiply(input1, input2)
 
 with tf.Session() as sess:
-    print (sess.run([output], feed_dict={input1:[7.], input2:[2.]}))
+    print (sess.run([output], feed_dict={input1:[7.,2.0], input2:[2.,3.0]}))
